a31926b16e288590d8704ce629d4632746207bfe
Kenji Salada <ksalada82524@liceo.edu.ph>
2025-08-18 15:13:47 +0800

    Activity4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Main branch");
+public static void main(String[] args) {
+        GradeCalculator calculator = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double[] johnGrades = {85.5, 92.0, 78.5, 90.0};
+        double average = calculator.calculateAverage(johnGrades);
+        String letterGrade = calculator.getLetterGrade(average);
+
+        System.out.println("--- Displaying Results for " + studentName + " ---");
+        calculator.displayResult(studentName, average);
+        calculator.displayResult(studentName, average, letterGrade);
+
     }
 }
-

ac9fe4dce3c29ad280b66d39d25a49c0c007fda1
Kenji V. Salada <ksalada82524@liceo.edu.ph>
2025-08-30 15:53:17 +0800

    lab-activity-5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main branch");
+        BankAccount account1 = new BankAccount("Marco", 1500.00);
+        BankAccount account2 = new BankAccount("Harun", 500.00);
+        BankAccount account3 = new BankAccount("Brian", 2500.00);
+
+        account1.deposit(200.00);
+        account2.withdraw(100.00);
+        account3.deposit(500.00);
+        account1.withdraw(2000.00);
+
+        System.out.println("\n--- Interest Calculation ---");
+        System.out.println("Interest for " + account1.accountHolderName + ": " + String.format("%.2f", account1.calculateInterest()));
+        System.out.println("Interest for " + account2.accountHolderName + ": " + String.format("%.2f", account2.calculateInterest()));
+        System.out.println("Interest for " + account3.accountHolderName + ": " + String.format("%.2f", account3.calculateInterest()));
+
+        account1.displayAccountInfo();
+        account2.displayAccountInfo();
+        account3.displayAccountInfo();
+
+        System.out.println("\nBank Name from static variable: " + BankAccount.bankName);
+        System.out.println("Total Accounts Created: " + BankAccount.totalAccounts);
     }
 }
-

2656ffe0b08db478a526dd69d4686251a37e8873
Kenji V. Salada <ksalada82524@liceo.edu.ph>
2025-09-22 17:36:07 +0800

    Update Trainable.java

--- src/Trainable.java
+++ src/Trainable.java
@@ -1,1 +1,10 @@
+interface Trainable {
+    void performTrick();
+
+    static void startTrainingSession() {
+        System.out.println("Training Session Started!");
+        System.out.println("===========================");
+        System.out.println();
+    }
+}
 

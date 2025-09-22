4a4ccef5a1212ae4165026128bb2c1aeb3f76cfd
Kenji V. Salada <ksalada82524@liceo.edu.ph>
2025-09-22 17:37:21 +0800

    Update pet.java

--- src/pet.java
+++ src/pet.java
@@ -1,1 +1,15 @@
+abstract class Pet {
+    String name;
+    int age;
 
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract void makeSound();
+
+    public void displayInfo() {
+        System.out.println("Pet Name: " + name + ", Age: " + age);
+    }
+}

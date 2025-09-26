6b0fa27a96100c38fa31528003a8d17381eda6eb
Kenji V. Salada <ksalada82524@liceo.edu.ph>
2025-09-26 18:55:05 +0800

    Create Media.java

---
+++ src/Media.java
@@ -0,0 +1,44 @@
+// Abstract class representing a media item
+public abstract class Media {
+    protected String title;
+    protected String itemId;
+    protected boolean isAvailable;
+
+    // Constructor
+    public Media(String title, String itemId) {
+        this.title = title;
+        this.itemId = itemId;
+        this.isAvailable = true;
+    }
+
+    // Concrete methods
+    public String getTitle() {
+        return title;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void borrowItem() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println(title + " has been borrowed");
+        } else {
+            System.out.println("Sorry, " + title + " is not available");
+        }
+    }
+
+    public void returnItem() {
+        isAvailable = true;
+        System.out.println(title + " has been returned");
+    }
+
+    // Abstract methods
+    public abstract String getMediaType();
+    public abstract void displayInfo();
+}

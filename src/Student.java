09818a9810aafc248b3aaca795f90ba4be85cbef
Kenji Salada <ksalada82524@liceo.edu.ph>
2025-10-02 15:57:48 +0800

    semi-final-exam

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+class Student extends User {
+    private String studentId;
+    private String major;
+
+    public Student(String userId, String name, String email, String studentId, String major) {
+        super(userId, name, email);
+        this.studentId = studentId;
+        this.major = major;
+    }
+
+    public int getMaxBorrowLimit() {
+        return 5;
+    }
+}
